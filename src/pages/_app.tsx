0ea2c7e8f526895b93bfbb2ebe5a3c8c4e0b8d66
lukasino1214 <lukasino1214@gmail.com>
2023-08-24 16:26:47 +0200

    profile page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
+
 import { api } from "~/utils/api";
+
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>t3-stack-test</title>
+        <meta name="description" content="💭" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,67 +1,99 @@
-import { clerkClient } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/dist/types/server";
+import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure, privateProcedure } from "~/server/api/trpc";
 
-const filterUserForClient = (user: User) => {
-    return { id: user.id, name: user.username, profilePicture: user.profileImageUrl };
-}
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
+import type { Post } from "@prisma/client";
+
+const addUserDataToPosts = async (posts: Post[]) => {
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })
+  ).map(filterUserForClient);
+
+  return posts.map((post) => {
+    const author = users.find((user) => user.id === post.authorId);
+
+    if (!author || !author.username)
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Author for post not found",
+      });
+
+    return {
+      post,
+      author: {
+        ...author,
+        username: author.username,
+      },
+    };
+  });
+};
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(3, "1 m"),
   analytics: true,
-  /**
-   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
-   * instance with other applications and want to avoid key collisions. The default prefix is
-   * "@upstash/ratelimit"
-   */ 
-  prefix: "@upstash/ratelimit",
 });
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
-        take: 100,
-        orderBy: [{createdAt: "desc"}]
+      take: 100,
+      orderBy: [{ createdAt: "desc" }],
     });
 
-    const users = (await clerkClient.users.getUserList({
-        userId: posts.map((post) => post.authorId),
-        limit: 100
-    })).map(filterUserForClient);
-
-    return posts.map(post => {
-        const author = users.find((user) => user.id == post.authorId);
-        if(!author) throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "author for post not found"});
-
-        return {
-            post,
-            author
-        };
-    });
+    return addUserDataToPosts(posts);
   }),
 
-  create: privateProcedure.input(z.object({
-    content: z.string().min(1).max(280)
-  })).mutation(async ({ctx, input}) => {
-    const authorId = ctx.userId;
-    const { success } = await ratelimit.limit(authorId);
+  getPostsByUserId: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+      })
+    )
+    .query(({ ctx, input }) =>
+      ctx.prisma.post
+        .findMany({
+          where: {
+            authorId: input.userId,
+          },
+          take: 100,
+          orderBy: [{ createdAt: "desc" }],
+        })
+        .then(addUserDataToPosts)
+    ),
 
-    if(!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS"});
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji("Only emojis are allowed").min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
 
-    const post = await ctx.prisma.post.create({
+      const { success } = await ratelimit.limit(authorId);
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+
+      const post = await ctx.prisma.post.create({
         data: {
-            authorId,
-            content: input.content,
+          authorId,
+          content: input.content,
         },
-    });
+      });
 
-    return post;
-  }),   
+      return post;
+    }),
 });
